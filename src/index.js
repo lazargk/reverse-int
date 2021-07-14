798a567a4bded3cc63405feda24fd818d9061907
Lazar Gumarov <54982041+lazargk@users.noreply.github.com>
2021-07-14 08:27:08 +0500

    feat: tast is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let res = "";
+  let str = "" + n;
+  const count = str.length; 
+  for (let i = 0; i < count; i++){
+      res = res + str[count - 1 - i];
+  }
+  return parseInt(res);
 }
